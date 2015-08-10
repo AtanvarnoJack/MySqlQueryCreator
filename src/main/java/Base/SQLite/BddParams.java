bac198b842634d6ed3a63077a94c56e23d756581
Alexandre <alexcornillon@gmail.com>
2015-08-10 16:53:23 +0200

    Architecture BddParams

--- src/main/java/Base/SQLite/BddParams.java
+++ src/main/java/Base/SQLite/BddParams.java
@@ -4,4 +4,11 @@ package Base.SQLite;
  * Created by alco on 10/08/2015.
  */
 public interface BddParams {
+    boolean createBase();
+
+    String getUserForUrl(String url);
+
+    String getCryptPassForUrl(String url);
+
+    void setConnection(String url, String user, String password);
 }
--- src/main/java/Base/SQLite/SQLJet/BddParamsSQLJet.java
+++ src/main/java/Base/SQLite/SQLJet/BddParamsSQLJet.java
@@ -6,5 +6,27 @@ import Base.SQLite.BddParams;
  * Created by alco on 10/08/2015.
  */
 public class BddParamsSQLJet implements BddParams {
+    @Override
+    public boolean createBase() {
+        boolean baseCreated = false;
+        
+        return baseCreated;
+    }
 
+    @Override
+    public String getUserForUrl(String url) {
+        String user = null;
+        return user;
+    }
+
+    @Override
+    public String getCryptPassForUrl(String url) {
+        String user = null;
+        return user;
+    }
+
+    @Override
+    public void setConnection(String url, String user, String password) {
+        OpenHelperBddParamsSQLJet openHelper = new OpenHelperBddParamsSQLJet();
+    }
 }
